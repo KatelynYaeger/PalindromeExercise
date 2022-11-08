b4a9db5dafad92a82d58b323fb73e824b93a349f
Katelyn Yaeger <knyaeger93@gmail.com>
2022-11-07 19:01:50 -0600

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsAPalindrome(string answer)
+        {
+            return answer.ToLower().SequenceEqual(answer.ToLower().Reverse());
+
+        }
+       
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("moM", true)]
+        [InlineData("Dumbo", false)]
+        [InlineData("jinglebells", false)]
+        [InlineData("TestseT", true)]
 
+        public void IsAPalindrome(string answer, bool expected)
+        {
+            var test = new WordSmith();
+
+            var actual = test.IsAPalindrome(answer);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
